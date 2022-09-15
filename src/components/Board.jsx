8d9b2b8545a45c4634b371d8b8433dda02fd25ab
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-14 21:35:57 -0700

    Board renders 8 Row components

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Row from './Row'
+import '../scss/board.scss'
 
 function Board(props) {
+
+  const rowCount = 8
+
   return (
     <div className="board">
-      Hello I am the board.
+      {Array(rowCount).fill('').map((row, rowIndex) => (
+        <Row
+          key={`row_${rowIndex}`}
+          rowIndex={rowIndex}
+        />
+      ))}
     </div>
   );
 }
