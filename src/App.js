3be86e7851447fe748271bb87b1e1762d4dd16f6
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-14 18:12:08 -0700

    Installed scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Board from './components/Board'
+import './scss/app.scss'
 
 function App() {
   return (
-    <Board />
+    <div className="container">
+      <Board />
+    </div>
   );
 }
 
