c996a78b35f442ad201e31b47edbc1ea7e464c8d
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-14 21:39:18 -0700

    Square component renders a color class, displays rowIndex and squareIndex values

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,12 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
+import '../scss/square.scss'
+import { isOdd } from '../utils/utils'
 
-class square extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
+function Square({ rowIndex, squareIndex }) {
+
+  const colorA = 'color-a'
+  const colorB = 'color-b'
+
+  const colorClass = () => {
+    const rowIndexIsOdd = isOdd(rowIndex)
+    const squareIndexIsOdd = isOdd(squareIndex)
+
+    return rowIndexIsOdd
+      ? squareIndexIsOdd
+        ? colorA
+        : colorB
+      : squareIndexIsOdd
+        ? colorB
+        : colorA
   }
+
+  return (
+    <div className={`square ${colorClass()}`}>
+      {`${rowIndex},${squareIndex}`}
+    </div>
+  );
 }
 
-export default square;
+export default Square;
