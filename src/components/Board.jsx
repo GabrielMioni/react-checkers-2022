0fc3d3561e65375d736845bf57c71ff798832a7c
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-15 18:15:34 -0700

    In Board checkers are initialized for both players

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import Row from './Row'
+import { initCheckers } from '../utils/board'
 import '../scss/board.scss'
 
 function Board(props) {
 
   const iterateValue = 8
+
+  const [checkersPlayerA, setCheckersPlayerA] = useState([])
+  const [checkersPlayerB, setCheckersPlayerB] = useState([])
+
+  useEffect(() => {
+    const checkersA = initCheckers('a', 0)
+    const checkersB = initCheckers('b', 5)
+    setCheckersPlayerA(checkersA)
+    setCheckersPlayerB(checkersB)
+    console.log(checkersPlayerA)
+    console.log(checkersPlayerB)
+  }, [])
 
   return (
     <div className="board">
