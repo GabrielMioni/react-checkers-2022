825dfc6d15614f1d0e436116b778bd15b08edcf3
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-15 20:30:10 -0700

    Cleanup in Square

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -4,37 +4,38 @@ import Checker from './Checker'
 import { isOdd } from '../utils/utils'
 import { useSelector } from 'react-redux'
 
-const squareHasChecker = (checkers, rowIndex, squareIndex) => {
+const findOccupyingChecker = (checkers, rowIndex, squareIndex) => {
   return  checkers.find(checker => {
     const { row: rowPosition, square: squarePosition } = checker.position
     return rowPosition === rowIndex && squarePosition === squareIndex
   })
 }
 
-function Square({ rowIndex, squareIndex }) {
-  const checkers = useSelector((state) => state.checkers.value)
-  const existingChecker = squareHasChecker(checkers, rowIndex, squareIndex)
-  const hasChecker = existingChecker !== undefined
+const colorClass = (rowIndex, squareIndex) => {
+  const rowIndexIsOdd = isOdd(rowIndex)
+  const squareIndexIsOdd = isOdd(squareIndex)
+
   const colorA = 'color-a'
   const colorB = 'color-b'
 
-  const colorClass = () => {
-    const rowIndexIsOdd = isOdd(rowIndex)
-    const squareIndexIsOdd = isOdd(squareIndex)
+  return rowIndexIsOdd
+    ? squareIndexIsOdd
+      ? colorA
+      : colorB
+    : squareIndexIsOdd
+      ? colorB
+      : colorA
+}
 
-    return rowIndexIsOdd
-      ? squareIndexIsOdd
-        ? colorA
-        : colorB
-      : squareIndexIsOdd
-        ? colorB
-        : colorA
-  }
+function Square({ rowIndex, squareIndex }) {
+  const checkers = useSelector((state) => state.checkers.value)
+  const occupyingChecker = findOccupyingChecker(checkers, rowIndex, squareIndex)
+  const hasChecker = occupyingChecker !== undefined
 
   return (
-    <div className={`square ${colorClass()}`}>
+    <div className={`square ${colorClass(rowIndex, squareIndex)}`}>
       { !hasChecker && `${rowIndex},${squareIndex}`}
-      { hasChecker && <Checker checker={existingChecker}/> }
+      { hasChecker && <Checker checker={occupyingChecker}/> }
     </div>
   );
 }
