7d18af365b9cf063ea3b127afcf68b718bba5781
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-15 20:20:13 -0700

    Square renders Checker if the square is currently occuped by a checker

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import '../scss/square.scss'
+import Checker from './Checker'
 import { isOdd } from '../utils/utils'
+import { useSelector } from 'react-redux'
+
+const squareHasChecker = (checkers, rowIndex, squareIndex) => {
+  return  checkers.find(checker => {
+    const { row: rowPosition, square: squarePosition } = checker.position
+    return rowPosition === rowIndex && squarePosition === squareIndex
+  })
+}
 
 function Square({ rowIndex, squareIndex }) {
-
+  const checkers = useSelector((state) => state.checkers.value)
+  const existingChecker = squareHasChecker(checkers, rowIndex, squareIndex)
+  const hasChecker = existingChecker !== undefined
   const colorA = 'color-a'
   const colorB = 'color-b'
 
@@ -22,7 +33,8 @@ function Square({ rowIndex, squareIndex }) {
 
   return (
     <div className={`square ${colorClass()}`}>
-      {`${rowIndex},${squareIndex}`}
+      { !hasChecker && `${rowIndex},${squareIndex}`}
+      { hasChecker && <Checker checker={existingChecker}/> }
     </div>
   );
 }
