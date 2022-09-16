ef93802ebc21708cd6e4447057de3dcb90c5c385
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-15 18:03:04 -0700

    Added utils/board.js, which includes initCheckers function

---
+++ src/utils/board.js
@@ -0,0 +1,35 @@
+import { isOdd } from './utils'
+
+const setInitialSquarePosition = (row) => {
+  return isOdd(row) ? 0 : 1
+}
+
+export const initCheckers = (player, startRow) => {
+  const checkers = []
+
+  let row = startRow
+  let square = setInitialSquarePosition(row)
+
+  let rowSquareCount = 0
+
+  while (checkers.length < 12) {
+    const checkerObject = {
+      player,
+      id: `${player}${checkers.length}`,
+      position: {
+        row,
+        square
+      }
+    }
+    checkers.push(checkerObject)
+    square = square + 2
+    ++rowSquareCount
+    if (rowSquareCount >= 4) {
+      rowSquareCount = 0
+      ++row
+      square = setInitialSquarePosition(row)
+    }
+  }
+
+  return checkers
+}
