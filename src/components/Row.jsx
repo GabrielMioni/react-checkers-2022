acad52f8a77367ee648dbba05b0472a0429eae7a
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-18 15:19:02 -0700

    Renamed Row rowIndex prop to row

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Square from './Square'
 import '../scss/row.scss'
 
-function Row({ rowIndex, iterateValue, clickSquare }) {
-
+function Row ({ row, iterateValue, clickSquare }) {
   return (
     <div className="row">
-      {Array(iterateValue).fill('').map((square, squareIndex) => (
+      {Array(iterateValue).fill('').map((squareObject, square) => (
         <Square
-          key={`square_${squareIndex}`}
-          rowIndex={rowIndex}
-          squareIndex={squareIndex}
+          key={`square_${square}`}
+          row={row}
+          square={square}
           clickSquare={clickSquare}/>
       ))}
     </div>
