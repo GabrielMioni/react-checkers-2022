68c715c3f147b14da3ffba15630223e3d8b801ba
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-18 16:45:03 -0700

    Added gameStateService.js

--- src/services/gameStateService.js
+++ src/services/gameStateService.js
@@ -1,8 +1,13 @@
-import {
-  findItemOccupyingSquare,
-  findMoveOccupyingSquare
-} from '../utils/game'
+import store from '../store'
+import { findCheckerByRowSquare } from '../utils/utils'
 import { useSelector } from 'react-redux'
+import { setActiveChecker, setAvailableMoves, setCheckers } from '../store/gameSlice'
+import {
+  checkForOpponentNeighbors,
+  findItemOccupyingSquare,
+  findMoveOccupyingSquare,
+  getCheckersAfterMove,
+  getNeighborSquares } from '../utils/game'
 
 export const GetMoveInSquare = (row, square) => {
   const moves = useSelector(state => state.game.availableMoves)
@@ -12,4 +17,33 @@ export const GetMoveInSquare = (row, square) => {
 export const GetCheckerInSquare = (row, square) => {
   const checkers = useSelector(state => state.game.checkers)
   return findItemOccupyingSquare(checkers, row, square)
+}
+
+export const GetAllCheckers = () => {
+  return useSelector(state => state.game.checkers)
+}
+
+export const GetActiveChecker = () => {
+  return useSelector(state => state.game.activeChecker)
+}
+
+export const GetAvailableMoves = () => {
+  return useSelector(state => state.game.availableMoves)
+}
+
+export const SetActiveChecker = (occupyingChecker) => {
+  store.dispatch(setActiveChecker(occupyingChecker))
+}
+
+export const SetAvailableMoves = (row, square, allCheckers) => {
+  const activeChecker = findCheckerByRowSquare(row, square, allCheckers)
+  const neighborSquares = getNeighborSquares({ row, square })
+  const moves = checkForOpponentNeighbors(neighborSquares, allCheckers, activeChecker)
+  store.dispatch(setAvailableMoves(moves))
+}
+
+export const SetCheckerMove = (row, square, activeChecker, allCheckers) => {
+  const updatedCheckers = getCheckersAfterMove(activeChecker, allCheckers, row, square)
+  store.dispatch(setCheckers(updatedCheckers))
+  store.dispatch(setAvailableMoves(null))
 }
