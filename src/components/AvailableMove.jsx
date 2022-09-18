f8ed49db506db4de9e104d904b66f8c399ed3b06
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-17 20:34:59 -0700

    Added AvailableMove component and style sheet

---
+++ src/components/AvailableMove.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../scss/available-move.scss'
+
+function AvailableMove() {
+  return (
+    <div className="available-move">
+    </div>
+  );
+}
+
+export default AvailableMove;
