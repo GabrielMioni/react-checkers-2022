87433fbcdb03d56d578859d6364c53f5a2f27728
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-18 17:03:14 -0700

    In AvailableMove, pass kill value to gameState.SetCheckerMove

--- src/components/AvailableMove.jsx
+++ src/components/AvailableMove.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import * as gameState from '../services/gameStateService' 
 import '../scss/available-move.scss'
 
-function AvailableMove ({ row, square }) {
+function AvailableMove ({ row, square, occupyingMove }) {
   const activeChecker = gameState.GetActiveChecker()
   const allCheckers = gameState.GetAllCheckers()
+  const kill = occupyingMove.kill
   return (
     <div
       className="available-move"
-      onClick={() => gameState.SetCheckerMove(row, square, activeChecker, allCheckers)}
+      onClick={() => gameState.SetCheckerMove(row, square, activeChecker, allCheckers, kill)}
     />
   );
 }
