f68248ad3e9668a2b1023b83dbe0e6b21e6f2fd9
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-25 14:35:39 -0700

    In Checker added clickChecker function

--- src/components/Checker.jsx
+++ src/components/Checker.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import store from '../store'
+import { setActiveChecker } from '../store/gameSlice'
 import '../scss/checker.scss'
 
+const clickChecker = (event, checker) => {
+  event.stopPropagation()
+  store.dispatch(setActiveChecker(checker))
+}
+
 function Checker ({ checker }) {
-  const { row, square } = checker
+  const { row, square, player } = checker
 
   return (
-    <div className={`checker player-${checker.player}`}>
+    <div
+      className={`checker player-${player}`}
+      onClick={e => clickChecker(e, checker)}>
       <div className="checker__piece">
         {`${row},${square}`}
       </div>
