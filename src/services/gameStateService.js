96de7d6e96040f1caf9edf10a72991dd547ae5d2
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-25 10:42:36 -0700

    Added initCheckers

--- src/services/gameStateService.js
+++ src/services/gameStateService.js
@@ -1,22 +1,21 @@
 import store from '../store'
 import { findCheckerByRowSquare } from '../utils/utils'
 import { useSelector } from 'react-redux'
-import { setActiveChecker, setAvailableMoves, setCheckers } from '../store/gameSlice'
-import {
-  checkForOpponentNeighbors,
-  findItemOccupyingSquare,
-  findMoveOccupyingSquare,
-  getCheckersAfterMove,
-  getNeighborSquares } from '../utils/game'
+import { setActiveChecker, setAvailableMoves, setCheckers, setGame } from '../store/gameSlice'
+import * as game from '../utils/game'
+
+export const initCheckers = () => {
+  store.dispatch(setGame())
+}
 
 export const GetMoveInSquare = (row, square) => {
   const moves = useSelector(state => state.game.availableMoves)
-  return findMoveOccupyingSquare(moves, row, square)
+  return game.findMoveOccupyingSquare(moves, row, square)
 }
 
 export const GetCheckerInSquare = (row, square) => {
   const checkers = useSelector(state => state.game.checkers)
-  return findItemOccupyingSquare(checkers, row, square)
+  return game.findItemOccupyingSquare(checkers, row, square)
 }
 
 export const GetAllCheckers = () => {
@@ -42,13 +41,13 @@ export const SetActiveChecker = (occupyingChecker) => {
 
 export const SetAvailableMoves = (row, square, allCheckers) => {
   const activeChecker = findCheckerByRowSquare(row, square, allCheckers)
-  const neighborSquares = getNeighborSquares({ row, square })
-  const moves = checkForOpponentNeighbors(neighborSquares, allCheckers, activeChecker)
+  const neighborSquares = game.getNeighborSquares({ row, square })
+  const moves = game.checkForOpponentNeighbors(neighborSquares, allCheckers, activeChecker)
   store.dispatch(setAvailableMoves(moves))
 }
 
 export const SetCheckerMove = (row, square, activeChecker, allCheckers, kill) => {
-  const updatedCheckers = getCheckersAfterMove(activeChecker, allCheckers, row, square, kill)
+  const updatedCheckers = game.getCheckersAfterMove(activeChecker, allCheckers, row, square, kill)
   store.dispatch(setCheckers(updatedCheckers))
   SetClearMoves()
 }
