d0ba17dca4d6707feab00fbd2ce4f9c99029bee2
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-26 19:33:56 -0700

    Removed unused clickSquare prop in Row

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Square from './Square'
 import '../scss/row.scss'
 
-function Row ({ row, iterateValue, clickSquare }) {
+function Row ({ row, iterateValue }) {
   return (
     <div className="row">
       {Array(iterateValue).fill('').map((squareObject, square) => (
         <Square
           key={`square_${square}`}
           row={row}
-          square={square}
-          clickSquare={clickSquare}/>
+          square={square} />
       ))}
     </div>
   );
