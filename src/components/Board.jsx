c93d8bf9a57d0c4cd41ce1df62ece0565297daf0
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-26 19:10:06 -0700

    Board dispatches getGame reducer

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Row from './Row'
-import { initCheckers } from '../services/gameStateService'
+import store from '../store'
+import { setGame } from '../store/gameSlice'
 import '../scss/board.scss'
 
 function Board () {
@@ -8,7 +9,7 @@ function Board () {
   const iterateValue = 8
 
   useEffect(() => {
-    initCheckers()
+    store.dispatch(setGame())
   }, [])
 
   return (
