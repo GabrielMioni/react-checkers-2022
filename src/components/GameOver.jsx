d3bd4a9fe3eed6700b4c068490d89f62912a272f
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-28 18:21:31 -0700

    Added GameOver component

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Dialog from './common/dialog/Dialog'
+import { useSelector } from 'react-redux'
+import Card from './common/card/Card'
+import store from '../store'
+import { resetGame } from '../store/gameSlice'
+
+function GameOver (props) {
+
+  const winner = useSelector(state => state.game.winner)
+
+  if (!winner) {
+    return
+  }
+
+  return (
+    <div className="game-over">
+      <Dialog>
+        <Card
+          height="250px"
+          toolbar="Congrats!">
+          <h3>Player '{ winner.toUpperCase() }' won!</h3>
+          <p>Would you like to play another game?</p>
+          <div>
+            <button
+              className="button"
+              onClick={() => store.dispatch(resetGame())}>
+              Play Again
+            </button>
+          </div>
+        </Card>
+      </Dialog>
+    </div>
+  );
+}
+
+export default GameOver;
