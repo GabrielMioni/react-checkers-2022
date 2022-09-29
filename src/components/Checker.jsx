d4f265b50116018fdbfa48c0485257f8bad277c6
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-28 19:49:22 -0700

    Kinged checkers display a crown

--- src/components/Checker.jsx
+++ src/components/Checker.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import store from '../store'
 import { setActiveChecker } from '../store/gameSlice'
+import crown from '../assets/crown.png'
 import '../scss/checker.scss'
 
 const clickChecker = (event, checker) => {
@@ -8,15 +9,25 @@ const clickChecker = (event, checker) => {
   store.dispatch(setActiveChecker(checker))
 }
 
+const renderCheckerContent = (isKing) => {
+  if (!isKing)
+    return null
+
+  return (
+    <img src={crown} alt="crown"/>
+  )
+}
+
 function Checker ({ checker }) {
-  const { row, square, player } = checker
+  const { row, square, player, isKing } = checker
 
   return (
     <div
       className={`checker player-${player}`}
       onClick={e => clickChecker(e, checker)}>
       <div className="checker__piece">
-        {`${row},${square}`}
+        { isKing && <img src={crown} alt="crown"/> }
+        { !isKing && `${row},${square}`}
       </div>
     </div>
   );
