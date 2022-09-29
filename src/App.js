f08888b042f81aa0a8b837dbeb2fbc59444eff2f
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-28 18:22:20 -0700

    App uses GameOver

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Board from './components/Board'
 import './scss/app.scss'
+import GameOver from './components/GameOver'
 
 function App () {
   return (
     <div className="container">
       <Board />
+      <GameOver />
     </div>
   );
 }
