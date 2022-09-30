eda5affac8a8fa9f9bdace5e3ba2af546d95de77
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-09-29 20:15:46 -0700

    Checker applies animation classes and dispatches to setCheckerMoved onAnimationEnd

--- src/components/Checker.jsx
+++ src/components/Checker.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import store from '../store'
-import { setActiveChecker } from '../store/gameSlice'
+import { setActiveChecker, setCheckerMoved } from '../store/gameSlice'
 import crown from '../assets/crown.png'
 import '../scss/checker.scss'
+import { useSelector } from 'react-redux'
 
 const clickChecker = (event, checker) => {
   event.stopPropagation()
@@ -15,13 +16,47 @@ const renderCheckerContent = (isKing, row, square) => {
     : `${row},${square}`
 }
 
+const buildCheckerClasses = (checker, activeChecker, selectedMove) => {
+  const { id, player } = checker
+  const activeId = activeChecker?.id
+
+  const isActive = id === activeId
+
+  let classesString = `checker player-${player}`
+  if (isActive) {
+    classesString += ' active'
+  }
+
+  if (!isActive) {
+    return classesString
+  }
+
+  if (selectedMove) {
+    const { movementId, kill } = selectedMove
+    classesString += ` moving-${movementId}`
+    if (kill) {
+      classesString += ' jump'
+    }
+  }
+
+  return classesString
+}
+
+const animationEnd = () => {
+  store.dispatch(setCheckerMoved())
+}
+
 function Checker ({ checker }) {
-  const { row, square, player, isKing } = checker
+  const { row, square, isKing } = checker
+
+  const selectedMove = useSelector(state => state.game.selectedMove)
+  const activeChecker = useSelector(state => state.game.activeChecker)
 
   return (
     <div
-      className={`checker player-${player}`}
-      onClick={e => clickChecker(e, checker)}>
+      className={buildCheckerClasses(checker, activeChecker, selectedMove )}
+      onClick={e => clickChecker(e, checker)}
+      onAnimationEnd={() => animationEnd()}>
       <div className="checker__piece">
         { renderCheckerContent(isKing, row, square) }
       </div>
