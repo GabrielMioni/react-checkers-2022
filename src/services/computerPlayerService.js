238734790a153f5ff7553fe443380ef24c428a4e
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-10-05 19:53:50 -0700

    Cleanup in computerPlayerService.js. getBestMove returns array objects with movementId included

--- src/services/computerPlayerService.js
+++ src/services/computerPlayerService.js
@@ -1,13 +1,12 @@
 import * as gameService from '../services/gameService'
 import { players } from './players'
-import checker from '../components/Checker'
 
 const movesArrayForChecker = (checker, checkers) => {
   const checkerMoveObject = gameService.getAvailableMoves(checker, checkers)
   return Object.keys(checkerMoveObject)
     .map(movementId => {
       const moveObject = checkerMoveObject[movementId]
-      return moveObject ? { ...moveObject, checkerId: checker.id } : null
+      return moveObject ? { ...moveObject, checkerId: checker.id, movementId } : null
     })
     .filter(move => move)
 }
@@ -30,38 +29,26 @@ export const getBestMove = (checkers, player) => {
   checkersWithPossibleMoves.map(checker => {
     const checkerMoves = movesArrayForChecker(checker, checkers)
 
-    // console.log(JSON.stringify(checker, null, 2))
-    // console.log(JSON.stringify(checkerMoves, null, 2))
-
     checkerMoves.map(move => {
-      const { row, square } = move
-      // const score = minimax(move, checker, checkers, true, 5)
-      const score = minimaxTwo(move, checkers, true, 4)
+      const { row, square, movementId } = move
+      const score = minimax(move, checkers, true, 4)
       results.push({
         checkerId: checker.id,
         row,
         square,
-        score
+        score,
+        movementId
       })
     })
   })
 
-  console.log(results)
   const bestScore = findBestScore(results)
 
   return bestScore ? bestScore : results[0]
 }
 
-const minimaxTwo = (move, checkers, maximizing, depth) => {
-
-  // console.log(JSON.stringify(checkers, null, 2))
-  // console.log({
-  //   move,
-  //   maximizing,
-  //   depth
-  // })
+const minimax = (move, checkers, maximizing, depth) => {
   let score = move.kill ? 10 : -Infinity
-  // console.log(score)
 
   const currentPlayer = maximizing ? players.b : players.a
   const playerWon = gameService.playerWon(currentPlayer, checkers)
@@ -70,9 +57,7 @@ const minimaxTwo = (move, checkers, maximizing, depth) => {
     score = score + 1000
   }
 
-  console.log({ score1: score })
   if (depth <= 0 || playerWon) {
-    // console.log({ score })
     return score
   }
 
@@ -80,28 +65,19 @@ const minimaxTwo = (move, checkers, maximizing, depth) => {
   const updatedCheckers = gameService.getCheckersAfterMove(move, activeChecker, checkers)
 
   // Switch players
-  // const opponentPlayer = maximizing ? players.a : players.b
   const opponentPlayer = currentPlayer === players.a ? players.b : players.a
   const opponentCheckersWithMoves = gameService.findCheckersWithPossibleMoves(opponentPlayer, updatedCheckers)
 
   opponentCheckersWithMoves.forEach(checker => {
-    // console.log(JSON.stringify(checker, null, 2))
     const checkerMoves = movesArrayForChecker(checker, checkers)
 
     checkerMoves.forEach(move => {
       const opponentActiveChecker = gameService.getCheckerById(move.checkerId, updatedCheckers)
       const opponentUpdatedCheckers = gameService.getCheckersAfterMove(move, opponentActiveChecker, updatedCheckers)
-
-      // console.log(move)
-      const currentDepth = depth -1
-      console.log({ currentDepth })
-      const opponentScore = minimaxTwo(move, opponentUpdatedCheckers, !maximizing, depth -1)
-      console.log({ opponentScore, depth })
+      const opponentScore = minimax(move, opponentUpdatedCheckers, !maximizing, depth -1)
       score = score - opponentScore
-      // console.log(score)
     })
   })
 
-  console.log({ score2: score })
   return score
 }
