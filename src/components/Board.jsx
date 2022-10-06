ce1e4929fb9b9eb7550372b788a67fa4979cc35b
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-10-05 19:52:50 -0700

    Board dispatches to setSelectedMove for computer moves

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import Row from './Row'
 import store from '../store'
-import { setGame } from '../store/gameSlice'
+import { setGame, setSelectedMove } from '../store/gameSlice'
 import '../scss/board.scss'
+import { useSelector } from 'react-redux'
 
 function Board () {
 
@@ -11,6 +12,16 @@ function Board () {
   useEffect(() => {
     store.dispatch(setGame())
   }, [])
+
+  const computerMove = useSelector(state => state.game.computerMove)
+  const availableMoves = useSelector(state => state.game.availableMoves)
+
+  if (computerMove && availableMoves) {
+    setTimeout(() => {
+      const selectedMove = availableMoves[computerMove.movementId]
+      store.dispatch(setSelectedMove(selectedMove))
+    }, 500)
+  }
 
   return (
     <div className="board">
