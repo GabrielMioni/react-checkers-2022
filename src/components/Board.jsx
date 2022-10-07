c0ef8e5558ee7aac9407976376c6fcd886bfef7d
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-10-06 20:16:18 -0700

    Moved logic related to starting a computer player's move from Board to BoardLogic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
 import Row from './Row'
 import store from '../store'
-import { setGame, setSelectedMove } from '../store/gameSlice'
+import { setGame } from '../store/gameSlice'
 import '../scss/board.scss'
-import { useSelector } from 'react-redux'
 
 function Board () {
 
@@ -12,16 +11,6 @@ function Board () {
   useEffect(() => {
     store.dispatch(setGame())
   }, [])
-
-  const computerMove = useSelector(state => state.game.computerMove)
-  const availableMoves = useSelector(state => state.game.availableMoves)
-
-  if (computerMove && availableMoves) {
-    setTimeout(() => {
-      const selectedMove = availableMoves[computerMove.movementId]
-      store.dispatch(setSelectedMove(selectedMove))
-    }, 500)
-  }
 
   return (
     <div className="board">
--- src/components/BoardLogic.jsx
+++ src/components/BoardLogic.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux'
-import { setComputerTurn } from '../store/gameSlice'
+import { setComputerTurn, setSelectedMove } from '../store/gameSlice'
 import { players } from '../services/players'
 import store from '../store'
 
@@ -8,6 +8,10 @@ function BoardLogic () {
   const computerIsPlaying = useSelector(state => state.game.computerPlayer)
   const currentPlayer = useSelector(state => state.game.currentPlayer)
 
+  /* **************************************************************************
+   * If the game is has a computer player and the current player is
+   * the computer, start the computer's turn
+   * **************************************************************************/
   useEffect(() => {
     if (!(computerIsPlaying && currentPlayer === players.b)) {
       return
@@ -17,6 +21,20 @@ function BoardLogic () {
     }, 0)
 
   }, [computerIsPlaying, currentPlayer])
+
+  /* **************************************************************************
+   * If a computer move is present, use it to find the available move and
+   * pass it to setSelectedMove
+   * **************************************************************************/
+  const computerMove = useSelector(state => state.game.computerMove)
+  const availableMoves = useSelector(state => state.game.availableMoves)
+
+  if (computerMove && availableMoves) {
+    setTimeout(() => {
+      const selectedMove = availableMoves[computerMove.movementId]
+      store.dispatch(setSelectedMove(selectedMove))
+    }, 500)
+  }
 
   return null
 }
