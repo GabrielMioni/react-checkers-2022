6e7caecea4e80c3f550e7705d9b2df7161615a62
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-10-06 19:51:23 -0700

    App uses BoardLogic

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Board from './components/Board'
 import './scss/app.scss'
 import GameOver from './components/GameOver'
+import BoardLogic from './components/BoardLogic'
 
 function App () {
   return (
     <div className="container">
+      <BoardLogic />
       <Board />
       <GameOver />
     </div>
